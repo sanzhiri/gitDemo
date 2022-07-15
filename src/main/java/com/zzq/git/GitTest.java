a00c5b2fbfcfbaa385311e74c3108ff7a1eb885f
sanzhiri <1280415211@qq.com>
2022-07-15 10:48:00 +0800

    push commit2

--- src/main/java/com/zzq/git/GitTest.java
+++ src/main/java/com/zzq/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello,git-master");
         System.out.println("hello,git-hot-fix");
         System.out.println("hello,github！");
+        System.out.println("ssh push");
     }
 }
